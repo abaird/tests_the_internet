9f46b18ae439fe169296f0f3dbde934630f89fea
Alan Baird <alan.baird@livingsocial.com>
2016-03-26 20:25:42 -0500

    first passing test - checkboxes

--- spec/checkboxes_spec.rb
+++ spec/checkboxes_spec.rb
@@ -1,7 +1,23 @@
 require 'rspec'
 
+
 describe 'checkboxes' do
+  before(:each) do
+    @browser = Watir::Browser.new
+    @browser.goto base_url + '/checkboxes'
+  end
+
+  after(:each) do
+    @browser.close unless @browser.nil?
+  end
+
+  let(:checkbox1) { @browser.checkbox(index: 0) }
+  let(:checkbox2) { @browser.checkbox(index: 1) }
+
   it 'should check both checkboxes' do
-    true.should == false
+    checkbox1.set
+    checkbox2.clear
+    expect(checkbox1.set?).to eq true
+    expect(checkbox2.set?).to eq false
   end
 end
