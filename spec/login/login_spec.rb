f5cb1325d607d96baeba1c59e0cd889a0844fe67
Alan Baird <abaird@bairdsnet.net>
2016-03-29 11:42:29 -0500

    reorganize and add login tests

---
+++ spec/login/login_spec.rb
@@ -0,0 +1,25 @@
+describe 'stuff' do
+  before(:each) do
+    @browser = Watir::Browser.new
+    @browser.goto base_url + '/login'
+  end
+
+  after(:each) do
+    @browser.close unless @browser.nil?
+  end
+
+  it 'should login' do
+    login_page = Login.new(@browser)
+    expect(login_page.title).to eq "The Internet"
+    login_page.login('tomsmith', "SuperSecretPassword!")
+    secure_area = SecureArea.new(@browser)
+    expect(secure_area.logout).to be_exists
+  end
+
+  it 'fails to login with invalid password' do
+    login_page = Login.new(@browser)
+    expect(login_page.title).to eq "The Internet"
+    expect { login_page.login('xyz', 'invalid') }.to raise_error RuntimeError
+  end
+end
+t
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require 'watir-webdriver'
+require 'tests_the_internet'
 
 RSpec.configure do |config|
   def base_url
